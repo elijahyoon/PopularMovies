4a98fe65d7b97ecb22239c40f55cb036d5118208
Elijah Yoon <elijahyoon@gmail.com>
2015-08-25 13:39:54 -0400

    ViewHolder pattern

---
+++ app/src/main/java/com/elijahyoon/popularmovies/MovieAdapter.java
@@ -0,0 +1,59 @@
+package com.elijahyoon.popularmovies;
+
+import android.app.Activity;
+import android.media.Image;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+/**
+ * Created by elijahyoon on 8/24/15.
+ */
+public class MovieAdapter extends ArrayAdapter<Movie> {
+
+    private static final String LOG_TAG = MovieAdapter.class.getSimpleName();
+
+    public MovieAdapter(Activity context, List<Movie> movies) {
+        super(context, 0, movies);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Movie movie = getItem(position);
+
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item_movie, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.movieTitle.setText(movie.getTitle());
+        Log.d(LOG_TAG,"Attaching image " + movie.getPoster());
+        Picasso.with(getContext()).load("http://i.imgur.com/DvpvklR.png").into(viewHolder.moviePoster);
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        private TextView movieTitle;
+        private ImageView moviePoster;
+
+        public ViewHolder(View view) {
+            movieTitle = (TextView) view.findViewById(R.id.grid_item_text);
+            moviePoster = (ImageView) view.findViewById(R.id.grid_item_icon);
+        }
+    }
+}
